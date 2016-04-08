2a8251b72ddeab8821c7fe742a1a5ed7ab094c83
Marissa Allen <marissa@dali.dartmouth.edu>
2016-04-07 22:32:03 -0600

    styling about and skills

--- js/main.js
+++ js/main.js
@@ -66,24 +66,4 @@
     variant: 'featherlight-gallery'
   });
 
-  //youtube video
-  //   var v = document.getElementsByClassName("youtube-player");
-  //   for (var n = 0; n < v.length; n++) {
-  //       var p = document.createElement("div");
-  //       p.innerHTML = labnolThumb(v[n].dataset.id);
-  //       p.onclick = labnolIframe;
-  //       v[n].appendChild(p);
-  //   }
-  //
-  // function labnolThumb(id) {
-  //     return '<img class="youtube-thumb" src="//i.ytimg.com/vi/' + id + '/hqdefault.jpg"></img><div class="play-button"></div>';
-  // }
-  //
-  // function labnolIframe() {
-  //     var iframe = document.createElement("iframe");
-  //   iframe.setAttribute("src", "//www.youtube-nocookie.com/embed/" + this.parentNode.dataset.id + "?autoplay=1&rel=0&amp&showinfo=0");
-  //     iframe.setAttribute("frameborder", "0");
-  //     iframe.setAttribute("id", "youtube-iframe");
-  //     this.parentNode.replaceChild(iframe, this);
-  // }
 })(jQuery);
